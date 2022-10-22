d472be4108b8762a8527c4ab45ba3a97aa557346
Piotr Berezka <berezkapiotr@gmail.com>
2022-10-22 14:04:38 +0200

    add docker and simple endpoint

--- backend/simple-survey/src/index.ts
+++ backend/simple-survey/src/index.ts
@@ -1,1 +1,15 @@
-console.log("xd");
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req: any, res: any) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
